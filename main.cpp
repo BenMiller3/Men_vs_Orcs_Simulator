bc8bcbb1cad437fe12696a397aea19e61e7b5edd
Ben Miller <benjaminmiller229@gmail.com>
2016-01-29 06:54:01 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+	default_random_engine randNum(time(NULL));
+	uniform_real_distribution<float> attack(0.0f, 3.0f);
+
+	int men;
+	int orcs;
+
+	float manHealth = 10.0f;
+	float orcHealth = 3.5f;
+
+	cout << "*** Man vs. Orc Battle Simulator ***\n\n";
+	cout << "How many men are you bringing into battle, general: ";
+	cin >> men;
+
+	cout << "\nHow many orcs are we up against, general: ";
+	cin >> orcs;
+
+	int totalMen = men;
+	int totalOrcs = orcs;
+
+	cout << "\n\n*Battle has commenced*\n";
+
+	// simulation occurs
+	while (men > 0 && orcs > 0) {
+		float orcAttack = attack(randNum);
+		float manAttack = attack(randNum);
+
+		manHealth -= orcAttack;
+		orcHealth -= manAttack;
+
+		if (orcHealth <= 0) { orcs -= 1; orcHealth = 3.5f; }
+		if (manHealth <= 0) { men -= 1; manHealth = 10.0f; }
+	}
+
+	cout << "\n*The battle has finished*\n";
+
+	cout << "\n\n*** CASUALTY REPORT ***\n";
+	cout << "Total men dead: " << totalMen - men << endl;
+	cout << "Total orcs dead: " << totalOrcs - orcs << endl;
+
+	if (men > orcs) {
+		cout << "\nGeneral, This is a battle we can win" << endl;
+	}
+
+	else {
+		cout << "\nGeneral, I would not put these men in this fight" << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
