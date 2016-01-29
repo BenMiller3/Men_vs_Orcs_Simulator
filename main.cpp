46c729223220e70ab28e2fcd9b19c947e23f400f
Ben Miller <benjaminmiller229@gmail.com>
2016-01-29 06:55:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@
 using namespace std;
 
 int main() {
-
+	
+	// Random Number Generator
 	default_random_engine randNum(time(NULL));
 	uniform_real_distribution<float> attack(0.0f, 3.0f);
-
+	
+	// Total men and orcs, declared by user
 	int men;
 	int orcs;
 
@@ -23,12 +25,13 @@ int main() {
 	cout << "\nHow many orcs are we up against, general: ";
 	cin >> orcs;
 
+	// Stores the values, as the other variables are altered
 	int totalMen = men;
 	int totalOrcs = orcs;
 
 	cout << "\n\n*Battle has commenced*\n";
 
-	// simulation occurs
+	// Simulation occurs
 	while (men > 0 && orcs > 0) {
 		float orcAttack = attack(randNum);
 		float manAttack = attack(randNum);
@@ -39,7 +42,8 @@ int main() {
 		if (orcHealth <= 0) { orcs -= 1; orcHealth = 3.5f; }
 		if (manHealth <= 0) { men -= 1; manHealth = 10.0f; }
 	}
-
+	
+	// End game sequence
 	cout << "\n*The battle has finished*\n";
 
 	cout << "\n\n*** CASUALTY REPORT ***\n";
